5a3c5bd22aa0926d305eb9665abd8a8c62090875
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-25 10:03:24 -0300

    commit del front end

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
