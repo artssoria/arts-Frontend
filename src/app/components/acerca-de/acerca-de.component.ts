e050baae47c60f5a5958961e53a7cfaddfa9b5af
artssoria <53129227+artssoria@users.noreply.github.com>
2022-08-02 23:25:08 -0300

    arreglos menores

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -9,11 +9,12 @@ import { PersonaService } from 'src/app/service/persona.service';
 })
 export class AcercaDeComponent implements OnInit {
   persona: persona = new persona("","","");
-
+  
   constructor(public personaService: PersonaService) { }
 
   ngOnInit(): void {
-    this.personaService.getpersona().subscribe(data => {this.persona = data})
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
   }
+  isLogged = false;
 
 }
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -3,14 +3,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { persona } from '../model/persona.model';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
   URL = 'https://backendsoria.herokuapp.com/personas/';
-  constructor(private http: HttpClient) { }
-  public getpersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL + 'traer/perfil');
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.httpClient.get<persona>(this.URL+'traer/perfil')
+  }
+
+  public detail(id: number): Observable<persona> {
+    return this.httpClient.get<persona>(this.URL + `detail/${id}`);
+  }
+
+  public save(persona: persona): Observable<any> {
+    return this.httpClient.post<any>(this.URL + 'create', persona);
+  }
+
+  public update(id: number, persona: persona): Observable<any> {
+    return this.httpClient.put<any>(this.URL + `update/${id}`, persona);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
   }
 }
